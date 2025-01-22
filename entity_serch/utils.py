e01c5f32fab6852179f650c56e3fadb104968aa5
ErnestoAizenberg <sereernest@gmail.com>
2025-01-22 18:43:29 +0300

    Create utils.py

---
+++ entity_serch/utils.py
@@ -0,0 +1,19 @@
+import secrets
+import string
+import os
+from flask import flash
+
+
+def generate_random_password(length=12):
+    characters = string.ascii_letters + string.digits
+    password = ''.join(secrets.choice(characters) for i in range(length))
+    return password
+
+
+def download_file(file, file_path):
+    try:
+        file.save(file_path)
+        return file_path
+    except Exception as e:
+        print('Duuring the process of loading file an exception has occurred:', e)
+        return None￼Enter
